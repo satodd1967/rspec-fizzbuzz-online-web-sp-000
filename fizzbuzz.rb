92c4ad9fe6bc5c196c64c722ccdca92e92370e20
Seth Todd <seth.todd@outlook.com>
2019-11-24 23:49:56 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,5 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  if int % 5 and int % 3 == 0
-    "Fizzbuzz"
-  elsif int % 5 == 0
-    "Buzz"
-  elsif int % 3 == 0
-    "Fizz"
-    end
+  if int % 3
 end
