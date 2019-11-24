aa2dec3ba597d5b97f7e20cb14c983b48ba5ac73
Seth Todd <seth.todd@outlook.com>
2019-11-24 23:45:00 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,10 +2,10 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if int % 3 == 0
-    "Fizz"
+    puts "Fizz"
   elsif int % 5 == 0
-    "Buzz"
+    puts "Buzz"
   else int % 3 && 5 == 0
-    "Fizzbuzz"
+    puts "Fizzbuzz"
   end
 end
